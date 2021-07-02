364ab92f89111f7bc5a17a4c677c59d244952d4a
Oli Sturm <oliver@oliversturm.com>
2021-07-02 13:57:49 +0100

    no more space cadet shift - never used it, quite irritating

--- keyboards/massdrop/ctrl/keymaps/oli/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/oli/keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC,   KC_HOME,  KC_HOME, KC_PGUP, \
         KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
         TD(TD_CAPS_CURSOR), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, KC_ENT, \
-        KC_LSPO, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSPC,                              KC_UP, \
+        KC_LSHIFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSHIFT,                              KC_UP, \
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(LAYOUT_STANDARD_FN),   /*TEST_OUTPUT_LAYER*/OSL(LAYOUT_SPECIAL_CHARS),  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [LAYOUT_STANDARD_FN] = LAYOUT(
